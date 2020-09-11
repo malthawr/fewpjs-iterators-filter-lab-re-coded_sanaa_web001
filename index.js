50c664c1895d6505e695e38930f0f2d77813278f
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-11 05:19:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(x,y){
+  let y2 = y.toLowerCase();
+  let result = x.filter(ele => 
+  {return ele ===y || ele ===y2;});
+  return result;
+}
+
+function fuzzyMatch(x,y){
+  let result = x.filter(ele =>  {
+ return (ele.charAt(0) === y.charAt(0) && ele.charAt(1) ===y.charAt(1));});
+  return result; 
+}
+
+function matchName(x,y){
+  let result = x.filter(ele =>  {
+ if(ele.name === y)
+    return ele;
+  });
+  return result;
+} 
